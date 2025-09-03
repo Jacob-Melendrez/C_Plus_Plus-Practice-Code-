cb27685d8dc9ed914799f5e2b127b9a89401fd30
Jacob Melendrez <jacob.n.melendrez@gmail.com>
2025-09-02 21:36:16 -0400

    Add files via upload

---
+++ unsigned_integer.cpp
@@ -0,0 +1,32 @@
+// A town's population has a value between 10 and 50,000. 
+//Declare townPopulation as a variable of type unsigned short.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+unsigned short townPopulation; 
+
+   // Assign townPopulation with various values. Each unsigned value ends with u
+cout << "\n";
+cout << "Population totals: " << endl;
+cout << "\n";
+
+    townPopulation = 26216;
+cout << townPopulation << endl;
+
+    townPopulation = 10244;
+cout << townPopulation << endl;
+
+    townPopulation = 10;
+cout << townPopulation << endl;
+
+    townPopulation = 50000;
+cout << townPopulation << endl;
+cout << "\n";
+
+
+
+return 0;
+}
