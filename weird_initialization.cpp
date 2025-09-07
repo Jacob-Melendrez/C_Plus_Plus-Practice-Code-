b12b77db0d0056f1e3cbe967fdc766e73dff4faf
Jacob Melendrez <jacob.n.melendrez@gmail.com>
2025-09-07 18:30:13 -0400

    Add files via upload

---
+++ weird_initialization.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+double x(2.5);
+double y(4.5);
+
+cout << "\n";
+cout << x << endl;
+cout << "\n";
+cout << y << endl;
+cout << "\n";
+
+return 0;
+}
