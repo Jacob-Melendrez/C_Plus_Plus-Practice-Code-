a38b5c9f9188a5cd7233647c5a9e7bd4fb29ec48
Jacob Melendrez <jacob.n.melendrez@gmail.com>
2025-09-07 18:25:06 -0400

    Add files via upload

---
+++ increment_decrement.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 1;
+    int j = 1;
+
+// Increment Practice 
+cout << "\n";
+cout << "This is the value i before post increment. " << endl;
+cout << i << endl;
+cout << "\n";
+
+i++;
+
+cout << "This is the value after the variable i after post increment of 1. " << endl;
+cout << i << endl;
+cout << "\n";
+
+
+
+// Decrement practice. 
+cout << "\n";
+cout << "This is the value i before post decrement. " << endl;
+cout << j << endl;
+cout << "\n";
+
+j--;
+
+cout << "This is the value after the variable i after post decrement of 1. " << endl;
+cout << j << endl;
+cout << "\n";
+
+return 0;
+}
