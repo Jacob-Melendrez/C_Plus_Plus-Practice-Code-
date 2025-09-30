c1e6946ab80ddf8d8b5f6bd3f4b2df164e62c6f5
Jacob Melendrez <jacob.n.melendrez@gmail.com>
2025-09-30 12:27:41 -0400

    Add files via upload

---
+++ variable_scope_practice.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+
+int main() {
+
+int k;
+    for (k = 1; k <= 8; k++)
+    {
+    cout << log(double(k)) 
+    << endl;
+    }
+    cout << k << endl;
+
+return 0;
+}
